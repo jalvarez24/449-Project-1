7078623d8220a4ec516e00d5950d2d22f815fa34
jalvarez24 <jayroalvarez@csu.fullerton.edu>
2019-10-12 18:25:47 -0700

    Fixed foreign key constraint errors. Fully functional now.

--- musicService.sql
+++ musicService.sql
@@ -1,14 +1,20 @@
--- $ sqlite3 books.db < sqlite.sql
 
-PRAGMA foreign_keys = OFF;
+PRAGMA foreign_keys = ON;
+
 BEGIN TRANSACTION;
-DROP TABLE IF EXISTS Track;
-DROP TABLE IF EXISTS Playlist;
+
 DROP TABLE IF EXISTS Tracks_List;
+DROP TABLE IF EXISTS Playlist;
 DROP TABLE IF EXISTS Description;
+DROP TABLE IF EXISTS Track;
+DROP TABLE IF EXISTS User;
+
+
+DROP TABLE IF EXISTS User;
+
 
 CREATE TABLE Track (
-    track_id INTEGER PRIMARY KEY,
+    track_id INTEGER primary key,
     track_title VARCHAR,
     album_title VARCHAR,
     artist VARCHAR,
@@ -18,11 +24,21 @@ CREATE TABLE Track (
     UNIQUE(track_title, artist)
 );
 
-INSERT INTO Track(track_title, album_title, artist, length_seconds, url_media, url_art) VALUES('Under Pressure', 'Hot Space', 'Queen', 191, 'www.soundcloud.com/songExample1', 'www.www.flickr.com/img1');
-INSERT INTO Track(track_title, album_title, artist, length_seconds, url_media, url_art) VALUES('Stan', 'The Marshall Mathers LP','Eminem', 180, 'www.soundcloud.com/songExample2', 'www.www.flickr.com/img2');
-INSERT INTO Track(track_title, album_title, artist, length_seconds, url_media, url_art) VALUES('Sunflower', 'Spiderman:Spiderverse', 'Post Malone', 187, 'www.soundcloud.com/songExample3', 'www.www.flickr.com/img3');
+CREATE TABLE Playlist (
+    playlist_id INTEGER primary key,
+    playlist_title VARCHAR,
+    description VARCHAR,
+    username_id VARCHAR,
+    FOREIGN KEY (username_id) REFERENCES User(username)
+);
 
-DROP TABLE IF EXISTS User;
+CREATE TABLE Tracks_List (
+    playlist_id INTEGER,
+    track_id INTEGER,
+    FOREIGN KEY (playlist_id) REFERENCES Playlist(playlist_id),
+    FOREIGN KEY (track_id) REFERENCES Track(track_id)
+);
+
 CREATE TABLE User (
   username VARCHAR PRIMARY KEY,
   password VARCHAR,
@@ -32,42 +48,47 @@ CREATE TABLE User (
   UNIQUE(username, email)
 );
 
-
-INSERT INTO User(username, password, display_name, email, homepage_url)
-  VALUES('noobmaster69', 'mySecretPassword', 'noooob', 'thegreatnoob@gmail.com', 'https://thenoob.com');
-INSERT INTO User(username, password, display_name, email, homepage_url)
-  VALUES('bob42', 'noPass', 'GumsGalore', 'oldDood@gmail.com', 'https://theDood.com');
-
-
-CREATE TABLE Playlist (
-    playlist_id INTEGER PRIMARY KEY,
-    playlist_title VARCHAR,
-    description VARCHAR,
-    username_id VARCHAR
-);
-
-INSERT INTO Playlist(playlist_title, description, username_id) VALUES('Tables are Hard', 'When the times are tough', 'noobmaster69');
-INSERT INTO Playlist(playlist_title, description, username_id) VALUES('MyPlaylistTitle', '', 'bob42');
-INSERT INTO Playlist(playlist_title, description, username_id) VALUES('NewPlaylist', '', 'noobmaster69');
-
-
-CREATE TABLE Tracks_List (
-	list_id INTEGER PRIMARY KEY,
-	playlist_id INTEGER,
-	track_title VARCHAR
-);
-
-INSERT INTO Tracks_List(playlist_id, track_title) VALUES (4, 'Stan');
-INSERT INTO Tracks_List(playlist_id, track_title) VALUES (4, 'Sunflower');
-INSERT INTO Tracks_List(playlist_id, track_title) VALUES (5, 'Under Pressure');
-
-
 CREATE TABLE Description (
-  username VARCHAR PRIMARY KEY,
-  track_id INTEGER,
-  description_text VARCHAR
+  username VARCHAR NOT NULL,
+  track_id INTEGER NOT NULL,
+  description_text VARCHAR,
+  FOREIGN KEY (username) REFERENCES User(username),
+  FOREIGN KEY (track_id) REFERENCES Track(track_id)
 );
+
+---------------------------------------------------------------
+--SECTION TO INSERT SAMPLE DATA TO START DB WITH--
+---------------------------------------------------------------
+
+--Inserting sample tracks
+INSERT INTO Track(track_title, album_title, artist, length_seconds, url_media, url_art) VALUES('Under Pressure', 'Hot Space', 'Queen', 191, 'www.soundcloud.com/songExample1', 'www.www.flickr.com/img1');
+INSERT INTO Track(track_title, album_title, artist, length_seconds, url_media, url_art) VALUES('Stan', 'The Marshall Mathers LP','Eminem', 180, 'www.soundcloud.com/songExample2', 'www.www.flickr.com/img2');
+INSERT INTO Track(track_title, album_title, artist, length_seconds, url_media, url_art) VALUES('Sunflower', 'Spiderman:Spiderverse', 'Post Malone', 187, 'www.soundcloud.com/songExample3', 'www.www.flickr.com/img3');
+
+--Inserting sample users
+INSERT INTO User(username, password, display_name, email, homepage_url)
+  VALUES('jayro123', 'mySecretPassword', 'jayroAY', 'jayro@gmail.com', 'https://jayro123.com');
+INSERT INTO User(username, password, display_name, email, homepage_url)
+  VALUES('ian123', 'mySecretPassword', 'ianAY', 'ian@gmail.com', 'https://ian123.com');
+INSERT INTO User(username, password, display_name, email, homepage_url)
+  VALUES('brendan123', 'mySecretPassword', 'brendanAY', 'brendan@gmail.com', 'https://brendan123.com');
+
+--Inserting sample playlists (Can insert now that there is a user to link to)
+INSERT INTO Playlist(playlist_title, description, username_id) VALUES('Chill Playlist', 'For times to chill.', 'jayro123');
+INSERT INTO Playlist(playlist_title, description, username_id) VALUES('Dance Playlist', NULL, 'ian123');
+INSERT INTO Playlist(playlist_title, description, username_id) VALUES('Fire Playlist', 'It is going down tonight!', 'brendan123');
+
+--Inserting sample tracks intoTracks_List (Can insert now that there are playlists AND tracks to link to)
+INSERT INTO Tracks_List(playlist_id, track_id) VALUES(1, 1);
+INSERT INTO Tracks_List(playlist_id, track_id) VALUES(2, 2);
+INSERT INTO Tracks_List(playlist_id, track_id) VALUES(3, 3);
+
+--Inserting sample descriptions (Can insert now that there are users AND tracks to link to)
 INSERT INTO Description(username, track_id, description_text)
-  VALUES('noobmaster69', 1, 'Wow, what a lit track!');
+  VALUES('jayro123', 1, 'Wow, what a lit track!');
+INSERT INTO Description(username, track_id, description_text)
+  VALUES('ian123', 2, 'Not bad, not bad.');
+INSERT INTO Description(username, track_id, description_text)
+  VALUES('brendan123', 3, 'My fave!!! :)');
 
 COMMIT;
